ff345f6fa55e19baada0167964eddf00dfc07779
Prashant Hans <phprashanthans@gmail.com>
2025-02-15 12:12:38 +0530

    Update Strategy.cpp

--- Strategy.cpp
+++ Strategy.cpp
@@ -43,3 +43,52 @@ int main() {
     ctx.executeSort(nums);
     return 0;
 }
+----------------------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <memory>
+
+// Define a strategy interface
+class Strategy {
+    public:
+        virtual ~Strategy() = default;
+        virtual void execute() const = 0;
+};
+
+// Implement the execute method for different scenarios
+class OffensiveStrategy : public Strategy {
+    public:
+        void execute() const override {
+            std::cout << "Using offensive strategy! Attack !!!!\n";
+        }
+};
+class DefensiveStrategy : public Strategy {
+    public:
+        void execute() const override {
+            std::cout << "Using defensive strategy! Protective !!!\n";
+        }
+};
+class Player {
+    public:
+        Player(std::unique_ptr<Strategy> initialStrategy) :
+            strategy(std::move(initialStrategy)) {}
+        void setStrategy(std::unique_ptr<Strategy> newStrategy) {
+            strategy = std::move(newStrategy);
+        }
+        void play() const {
+            strategy->execute();
+        }
+    private:
+        std::unique_ptr<Strategy> strategy;
+};
+
+int main() {
+    Player player(std::make_unique<OffensiveStrategy>());
+    player.play();
+    player.setStrategy(std::make_unique<DefensiveStrategy>());
+    player.play();
+    return 0;
+}
+/*---------------------------------------------------------------------------------
+Using offensive strategy! Attack !!!!
+Using defensive strategy! Protective !!!
+----------------------------------------------------------------------------------*/
